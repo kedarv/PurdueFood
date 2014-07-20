97ea04d3cd12c1cad2212510991abf64e5217a04
Kedar <kedar54@gmail.com>
2014-07-20 14:41:03 -0500

    updated composer.json with exporter

---
+++ app/database/migrations/2014_07_20_193910_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateReviewsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('reviews', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('food_id', 45)->nullable();
+			$table->integer('user_id')->nullable();
+			$table->text('comment')->nullable();
+			$table->float('rating')->nullable();
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('reviews');
+	}
+
+}
