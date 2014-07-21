b51755ae30e597909080c8b6247e3b447a30007f
Kedar <kedar54@gmail.com>
2014-07-21 17:10:29 -0500

    ignore db changes

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'mapleblade',
+			'database'  => 'purduefood',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
