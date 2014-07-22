a51eacee7f94abcbec83e7eb1432bf07a1747442
Kedar <kedar54@gmail.com>
2014-07-21 20:00:40 -0500

    fixed hostname, gravatar

--- app/config/local_kedar/database.php
+++ app/config/local_kedar/database.php
@@ -23,9 +23,9 @@ return array(
         'mysql' => array(
             'driver'    => 'mysql',
             'host'      => 'localhost',
-            'database'  => 'purduefood',
+            'database'  => 'mapleblade',
             'username'  => 'root',
-            'password'  => 'root',
+            'password'  => '',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local_nicky' => array('nickysemenza.local'),
-    'local_kedar' => array('kedar hostname here'),
+    'local_kedar' => array('kedarPC'),
 
 ));
 
