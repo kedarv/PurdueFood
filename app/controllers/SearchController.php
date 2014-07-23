b06fc3af1f65dce449c25ff36ab52096a28e23f2
Kedar <kedar54@gmail.com>
2014-07-23 14:14:24 -0500

    remove searchByFood param

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -1,11 +1,13 @@
 <?php
 
 class SearchController extends Controller {
+	//Make Page
 	public function searchByDate(){
 		$data['name'] = "Search";
 		$data['current_date'] = date('m-d-Y', time());
 		return View::make('search', compact('data'));
 	}
+	//Validate Form data, redirect to DiningController@pushData if correct
 	public function redirectToDate() {
 		$validator = Validator::make(
 			array('date' => Input::get('date')),
@@ -22,7 +24,7 @@ class SearchController extends Controller {
 		}
 	}
 	
-	public function searchByFood($date = NULL){
+	public function searchByFood(){
 		
 	}
 }
