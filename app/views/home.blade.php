816cd104d7ccf3040fa218850e59c98e19492d33
Kedar <kedar54@gmail.com>
2014-07-28 11:14:32 -0500

    fixed js include

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -71,7 +71,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
+                    <p>Copyright &copy; PurdueFood.com</p>
                 </div>
             </div>
         </footer>
@@ -79,12 +79,5 @@
     </div>
     <!-- /.container -->
 
-    <!-- jQuery Version 1.11.0 -->
-    <script src="js/jquery-1.11.0.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
 </body>
-
 </html>
