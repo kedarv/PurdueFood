21154bc4595cc8f5a4e83b27fbd9d0fcafabda74
Nicky Semenza <14nicholasse@gmail.com>
2014-07-29 15:11:28 -0700

    added GA tracking codes

--- app/views/user/layout.blade.php
+++ app/views/user/layout.blade.php
@@ -10,6 +10,16 @@
 	{{ HTML::script('js/bootstrap.min.js'); }}	
 </head>
 <body>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-53348908-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
 <div class="container">
 	<div class="row" style="margin-top:50px;">
 		<div class="col-md-6 col-md-offset-3">
