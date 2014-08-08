77b179f0b651a4d1fe5baa24e8f4887642c9bfa3
root <root@purduefood.com>
2014-08-08 11:57:44 -0400

    fixed mail issues

--- app/commands/jsonemail.php
+++ app/commands/jsonemail.php
@@ -91,20 +91,22 @@ class jsonemail extends Command {
 		$allowedTo = User::where('settingToggle_allowemail', '=', 1)
 					->where('email', '!=', '')->select('id', 'email', 'firstname', 'lastname')->get()->toArray();
 		foreach($allowedTo as $allowed) {
-		$this->info('allowed foreach');
 			$favlist = Favorites::where('user_id', '=', $allowed['id'])->select('food_id')->get()->toArray();
-			$this->info(print_r($favlist));
-			$data = NextDay::whereIn('food_id', Favorites::where('user_id', '=', $allowed['id'])->select('food_id')->get()->toArray())->get()->toArray();
-			$this->info('next day query');
-				//$data = NextDay::where('food_id', '=', $next['food_id'])->select('food_name', 'hall', 'station', 'meal')->get()->toArray();
-				//foreach($data as $item) {
-					//$this->info($item['food_name'] . $item['meal'] . $allowed['id']);
-				//}
-			
-			Mail::send('user.emails.fav', compact('data'), function($message) use ($allowed)
-			{
-				$message->to($allowed['email'], ''.$allowed['firstname']. ' ' . $allowed['lastname'].'')->subject('Tomorrows Schedule!');
-			});
+			if(!empty($favlist)) {
+				$data = NextDay::whereIn('food_id', Favorites::where('user_id', '=', $allowed['id'])->select('food_id')->get()->toArray())->get()->toArray();
+				if(!empty($data)) {
+					$this->info('next day query');
+						//$data = NextDay::where('food_id', '=', $next['food_id'])->select('food_name', 'hall', 'station', 'meal')->get()->toArray();
+						//foreach($data as $item) {
+							//$this->info($item['food_name'] . $item['meal'] . $allowed['id']);
+						//}
+					
+					Mail::send('user.emails.fav', compact('data'), function($message) use ($allowed)
+					{
+						$message->to($allowed['email'], ''.$allowed['firstname']. ' ' . $allowed['lastname'].'')->subject('Tomorrows Schedule!');
+					});
+				}
+			}
 		}
 	}
 
