6dc7c78d0e25bd388016b4e308b48737f5ac422c
Nicky Semenza <14nicholasse@gmail.com>
2014-09-03 22:59:02 -0400

    all our keys are belong to us

--- app/config/keys.php
+++ app/config/keys.php
@@ -1,7 +1,7 @@
 <?php
 return array(
-  'mailgun' => 'key-2b1dxrtom76-hpnspdqqwjfdbnqy84m9',
+  'mailgun' => 'xxx',
   'fb_appid' => '309640359218030',
-  'fb_secret' => 'e80105d3f5ccdb973d2773f7c9204f6c',
-   'production_sqlpass'=>'rEgutHathu3R62pu'
+  'fb_secret' => 'xxx',
+   'production_sqlpass'=>'xxx'
 );
