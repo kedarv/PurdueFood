96de4fab667bd2e78a12acddf7a3ff6440ab9b2f
Kedar <kedar@pal-nat184-073-110.itap.purdue.edu>
2014-09-22 10:07:11 -0400

    changed to micro data

--- app/views/user/emails/fav.blade.php
+++ app/views/user/emails/fav.blade.php
@@ -1,15 +1,10 @@
-<script type="application/ld+json">
-{
-  "@context": "http://purduefood.com",
-  "@type": "EmailMessage",
-  "action": {
-    "@type": "ViewAction",
-    "url": "http://purduefood.com",
-    "name": "View Full Menu"
-  },
-  "description": "View the full Purdue Dining Court Menu"
-}
-</script>
+<div itemscope itemtype="http://schema.org/EmailMessage">
+  <div itemprop="action" itemscope itemtype="http://schema.org/ViewAction">
+    <link itemprop="url" href="https://purduefood.com"/>
+    <meta itemprop="name" content="View Full Menu"/>
+  </div>
+  <meta itemprop="description" content="View the full Purdue Dining Court Menu"/>
+</div>
 @foreach($data as $item)
 	{{$item['food_name']}} at {{$item['hall']}} during {{$item['meal']}}<br/>
 @endforeach
