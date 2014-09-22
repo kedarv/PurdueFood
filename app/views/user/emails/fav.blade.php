0f1e192cebada0c2252a41c5d860ea1f7e521566
Kedar <kedar@pal-nat184-073-110.itap.purdue.edu>
2014-09-22 09:56:01 -0400

    added schema.org button

--- app/views/user/emails/fav.blade.php
+++ app/views/user/emails/fav.blade.php
@@ -1,3 +1,15 @@
+<script type="application/ld+json">
+{
+  "@context": "http://purduefood.com",
+  "@type": "EmailMessage",
+  "action": {
+    "@type": "ViewAction",
+    "url": "http://purduefood.com",
+    "name": "View Full Menu"
+  },
+  "description": "View the full Purdue Dining Court Menu"
+}
+</script>
 @foreach($data as $item)
 	{{$item['food_name']}} at {{$item['hall']}} during {{$item['meal']}}<br/>
 @endforeach
